2df012c7a20922cdab8b40d23ce0ec328847f67d
Jhonatan Iberico <jhonatan.iberico@atheneasolutions>
2024-02-18 02:46:18 +0100

    fixed threejs render

--- src/app/three/three.page.ts
+++ src/app/three/three.page.ts
@@ -1,132 +1,280 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, NgZone, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { IceCream } from '../entities/ice-cream';
-
+import { GUI } from 'dat.gui';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'ice-cream-model',
   templateUrl: './three.page.html',
   styleUrls: ['./three.page.scss'],
 })
-export class ThreePage implements OnInit {
+export class ThreePage implements OnDestroy, OnInit{
+  @ViewChild('rendererCanvas', { static: true }) public rendererCanvas!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('canvasContainer', { static: true }) public canvasContainer!: ElementRef<HTMLCanvasElement>;
+
   @Input() set choice(val:IceCream[]){
-    if(val){
+    if(val && val != this.iceCreams){
       this.iceCreams = val;
-      this.init();
+      if(!this.iceCreamBall) setTimeout(() => {this.updateIceCream(); }, 500);
+      else this.updateIceCream();
     }
   }
   iceCreams:IceCream[] = [];
-  container: any;
 
-  constructor() { }
+  private canvas!: HTMLCanvasElement|null;
+  private scene!: THREE.Scene;
+  private environment!: RoomEnvironment;
+  private pmremGenerator!: THREE.PMREMGenerator;
+  private textureLoader!: THREE.TextureLoader;
 
-  ngOnInit() {
-    this.init();
+  private controls!: OrbitControls;
+  private renderer!: THREE.WebGLRenderer|null;
+  private camera!: THREE.PerspectiveCamera;
+  
+  private hemiLight!: THREE.HemisphereLight;
+  private spotLight!: THREE.SpotLight;
+  private shadowFloor!: THREE.Mesh;
+
+  private iceCreamBall!:THREE.Mesh;
+  private iceCreamAOTexture!: THREE.Texture;
+  private iceCreamHeightTexture!: THREE.Texture;
+
+  private frameId: number|null = null;  
+
+  private gui!: GUI;
+
+  constructor(private ngZone: NgZone) { }
+
+  ngOnInit(): void {
+    this.createScene(this.rendererCanvas);
+    this.animate();
   }
 
-  init(){
-    // if(this.container){
-    //   this.container.remove();
-    //   this.container = null;
-    // }
-    function render(){
-      renderer.render( scene, camera );
+  public ngOnDestroy(): void {
+    if (this.frameId != null) {
+      cancelAnimationFrame(this.frameId);
     }
-
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-  
-      renderer.setSize( window.innerWidth, window.innerHeight );
-
-      render();
+    if (this.renderer != null) {
+      this.renderer.dispose();
+      this.renderer = null;
+      this.canvas = null;
     }
+  }
 
-    this.container = document.getElementById('canvas');
-
-    //  create Renderer
-    const renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.toneMapping = THREE.ReinhardToneMapping;
-    renderer.toneMappingExposure = 2.3;
-    renderer.capabilities.getMaxAnisotropy();
-    this.container.appendChild(renderer.domElement);
+  public createScene(canvas: ElementRef<HTMLCanvasElement>): void {
+    // The first step is to get the reference of the canvas element from our HTML document
+    this.canvas = canvas.nativeElement;
+    
+    const width = this.canvasContainer.nativeElement.clientWidth;
+    const height = this.canvasContainer.nativeElement.clientHeight - 150;
+    
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true,    // transparent background
+      antialias: true // smooth edges
+    });
+    this.renderer.setClearColor( 0x000000, 0);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.toneMapping = THREE.ReinhardToneMapping;
+    this.renderer.toneMappingExposure = 2.3;
+    this.renderer.capabilities.getMaxAnisotropy();
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.renderer.setSize(width, height);
 
     //  create environment and scene
-    const environment = new RoomEnvironment( renderer );
-    const pmremGenerator = new THREE.PMREMGenerator( renderer );
-    const scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
+    this.environment = new RoomEnvironment( this.renderer );
+    this.pmremGenerator = new THREE.PMREMGenerator( this.renderer );
+    this.textureLoader = new THREE.TextureLoader();
+  
+    this.scene.environment = this.pmremGenerator.fromScene( this.environment ).texture;
+    this.environment.dispose();
 
-    scene.background = new THREE.Color( 0xded6d6 );
-    scene.environment = pmremGenerator.fromScene( environment ).texture;
-    environment.dispose();
+    this.addCamera(width, height);
+    this.addOrbitControls();
+    this.addLights();
+    this.addShadowFloor();
+    this.addIceCreamModel();
+    if(!environment.production) this.createGUIPanel();
+  }
 
-    //  define scene's camera
-    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 100 );
-    camera.position.set( -0.5, 0.5, 2 );
+  public animate(): void {
+    // We have to run this outside angular zones,
+    // because it could trigger heavy changeDetection cycles.
+    this.ngZone.runOutsideAngular(() => {
+      if (document.readyState !== 'loading') {        
+        this.render();
+      } else {
+        window.addEventListener('DOMContentLoaded', () => {
+          this.render();
+        });
+      }
 
-    //  define orbit controls that follows 3d object
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.addEventListener( 'change', render );
-    controls.target.set( 0, 0, 0);
-    controls.update();
-    controls.enablePan = false;
-    controls.enableDamping = true;
+      window.addEventListener('resize', () => {
+        this.resize();
+      });
+    });
+  }
 
-    //  LIGHTS
-    const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
-    scene.add(hemiLight);
+  public render(): void {
+    this.frameId = requestAnimationFrame(() => {
+      this.render();
+    });
 
-    const spotLight = new THREE.SpotLight(0xffa95c, 4);
-    spotLight.castShadow = true;
-    scene.add(spotLight);
+    if(this.renderer) this.renderer.render(this.scene, this.camera);
+  }
 
-    //  3D object loader
+  /**
+   * Resize the canvas element to the container new size
+   * @returns void
+   */
+  public resize() {
+    const canvas = this.renderer ? this.renderer.domElement : null;
+    if(!canvas)  return;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    if (canvas.width !== width || canvas.height !== height) {
+      if(this.renderer) this.renderer.setSize(width, height, false);
+      this.camera.aspect = width / height;
+      this.camera.updateProjectionMatrix();
+    }
+  }
+
+  /** 
+   * Define orbit controls that follows 3d object 
+  */
+  public addOrbitControls(){
+    this.controls = new OrbitControls( this.camera, this.renderer?.domElement );
+    this.controls.addEventListener( 'change', this.render );
+    this.controls.target.set( 0, 0, 0);
+    this.controls.maxPolarAngle = Math.PI - (Math.PI / 2);
+    this.controls.minPolarAngle = Math.PI / 3;
+    this.controls.minDistance = 2;
+    this.controls.maxDistance = 3;
+    this.controls.minAzimuthAngle = Math.PI * 1.75;
+    this.controls.maxAzimuthAngle = Math.PI * 2.25;
+    this.controls.enablePan = false;
+    this.controls.enableDamping = true;
+    this.controls.update();
+  }
+
+  /**
+   * Add lights to the scene
+   */
+  public addLights(){    
+    this.hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+    this.hemiLight.castShadow = true;
+    this.scene.add(this.hemiLight);
+
+    this.spotLight = new THREE.SpotLight(0xffa95c, 4);
+    this.spotLight.castShadow = true;
+    this.scene.add(this.spotLight);
+  }
+
+  /**
+   * Creates and adds the camera to the scene.
+   * In order to calculate the aspect ratio we need the width and the height of the display container
+   * 
+   * @param canvasWidth:number    Canvas container width
+   * @param canvasHeight:number   Canvas container height
+   */
+  public addCamera(canvasWidth:number, canvasHeight:number){
+    this.camera = new THREE.PerspectiveCamera( 
+      45, canvasWidth / canvasHeight, 0.1, 100 
+    );
+    // this.camera.position.set( -0.5, 0.5, 2 );
+    // this.camera.position.z = 5;
+    this.scene.add(this.camera);
+  }
+
+  /**
+   * Function to add a transparent floor but receiving the icecream model shadow
+   */
+  addShadowFloor(){
+    const texture = this.textureLoader.load('assets/textures/txt_floor_shadow.png');
+    this.shadowFloor = new THREE.Mesh(
+      new THREE.PlaneGeometry(),
+      new THREE.MeshBasicMaterial(
+        { map: texture, side: THREE.DoubleSide, transparent: true})
+    );
+    this.shadowFloor.position.y = -0.2;
+    this.shadowFloor.scale.setScalar(1.3);
+    this.shadowFloor.rotation.x = - Math.PI / 2;
+    this.scene.add(this.shadowFloor);
+  }
+
+  async addIceCreamModel(){
+   //  3D object loader
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath( 'assets/libs/draco/gltf/' );
 
     const loader = new GLTFLoader();
     loader.setDRACOLoader( dracoLoader );
     loader.setPath( 'assets/models/gltf/' );
-    loader.load( 'xixo-cup.glb',  function ( gltf ) {
-      scene.add( gltf.scene );
-      
-      render();
+    loader.load( 'xixo-cup.glb',  ( gltf ) => {
+      this.scene.add( gltf.scene );
+      this.render();
     } );
 
-    //  Load icecream textures
-    const textureLoader = new THREE.TextureLoader();
-
-    const iceCreamAOTexture = textureLoader.load('assets/textures/ice_cream/ice_cream_ao.jpg');
-    const iceCreamHeightTexture = textureLoader.load('assets/textures/ice_cream/ice_cream_height.png');
-
-    let colorVarName = this.iceCreams[0]?.baseColorVarName ?? this.iceCreams[0]?.baseColor;
-    console.log("Color var name: ", colorVarName);
-    
-    const color = getComputedStyle(document.documentElement).getPropertyValue(`--ion-color-${colorVarName}`);
-    console.log("COLOR: ", color);
-
+    this.iceCreamAOTexture = this.textureLoader.load('assets/textures/ice_cream/ice_cream_ao.jpg');
+    this.iceCreamHeightTexture = this.textureLoader.load('assets/textures/ice_cream/ice_cream_height.png');    
+  
     //  Create balls
-    let ball = new THREE.Mesh(
+    this.iceCreamBall = new THREE.Mesh(
       new THREE.SphereGeometry(.3),
       new THREE.MeshStandardMaterial({
-        aoMap: iceCreamAOTexture,
-        bumpMap: iceCreamHeightTexture,
+        aoMap: this.iceCreamAOTexture,
+        bumpMap: this.iceCreamHeightTexture,
         bumpScale: 0.05,
-        displacementMap: iceCreamHeightTexture,
+        displacementMap: this.iceCreamHeightTexture,
         displacementScale: 0.05,
-        color: new THREE.Color(color)
+        color: new THREE.Color(0xffffff)
       })
     );
-    ball.position.set(0, 0.15, 0);
-    scene.add(ball);
+    this.iceCreamBall.position.set(0, 0.15, 0);
+    this.scene.add(this.iceCreamBall);
+  }
 
-    window.addEventListener( 'resize', onWindowResize );
+  updateIceCream(){
+    let colorVarName = this.iceCreams[0]?.baseColorVarName ?? this.iceCreams[0]?.baseColor;
+    const color = getComputedStyle(document.documentElement).getPropertyValue(`--ion-color-${colorVarName}`);
+
+    this.iceCreamBall.material = new THREE.MeshStandardMaterial({
+      aoMap: this.iceCreamAOTexture,
+      bumpMap: this.iceCreamHeightTexture,
+      bumpScale: 0.05,
+      displacementMap: this.iceCreamHeightTexture,
+      displacementScale: 0.05,
+      color: new THREE.Color(color)
+    });
+  }
+
+  /**
+   * Creates GUI panel for controls only in developement mode
+   */
+  createGUIPanel(){
+    this.gui = new GUI();
+    const cubeFolder = this.gui.addFolder('Orbit Limits');
+    cubeFolder.add(this.controls, 'minPolarAngle', 0, Math.PI);
+    cubeFolder.add(this.controls, 'maxPolarAngle', 0, Math.PI);
+    cubeFolder.add(this.controls, 'minDistance', 0, 5);
+    cubeFolder.add(this.controls, 'minDistance', 0, 5);
+    cubeFolder.add(this.controls, 'minAzimuthAngle', -2 * Math.PI, 2 * Math.PI);
+    cubeFolder.add(this.controls, 'maxAzimuthAngle', -2 * Math.PI, 2 * Math.PI);
+    cubeFolder.open();
+    
+    const floorFolder = this.gui.addFolder('Floor Shadow');
+    floorFolder.add(this.shadowFloor.position, 'y', -1, 1);
+    floorFolder.add(this.shadowFloor.rotation, 'x', 0, Math.PI);
+    floorFolder.add({scalar: 1.25}, 'scalar', 0, Math.PI).onChange((val) => {this.shadowFloor.scale.setScalar(val)});
+
+    floorFolder.open();
   }
 
 }
